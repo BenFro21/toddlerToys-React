ab1855dfbf27e1760f9b30da31115e994ba59a3b
benfro21 <broad0601@gmail.com>
2023-02-06 12:53:08 -0500

    worked on toy detail page

--- src/pages/ToyPage/ToyPage.jsx
+++ src/pages/ToyPage/ToyPage.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const toyPage = ({toys}) => {
   return (
     <div>
+      <h1>Toy page</h1>
       <h1>{toys.title}</h1>
       <h2>{toys.price}</h2>
       <h3>{toys.description}</h3>
